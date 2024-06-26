a78064c1cf1cfa9059b12a19ee7382785f82dc58
EdisonBenavides <eabenavidesq@puce.edu.ec>
2024-06-25 21:53:14 -0500

    Se agrega la funcionalidad de que cuando se haga click se coloque 'X' en la cuadrícula

--- React-ticTacToe/src/App.jsx
+++ React-ticTacToe/src/App.jsx
@@ -1,24 +1,35 @@
-function Square({ value }) {
-  return <button className="square">{value}</button>
+import { useState } from "react";
+import "./App.css"
+
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick(){
+    setValue('X');
+  }
+
+  return(
+    <button className="square" onClick={handleClick}>{value}</button>
+  );
 }
 
 export default function Board(){
   return(
     <>
       <div className="board-row">
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value={6} />
-        <Square value={7} />
-        <Square value={8} />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
